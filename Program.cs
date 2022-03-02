e44bc0c8bc59b0181559c097e46728f61bcebc54
madoka93 <madoka9891@gmail.com>
2022-03-02 12:56:04 +0900

    test
    test

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test_dot_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
